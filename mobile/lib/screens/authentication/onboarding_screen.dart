442435fb4d36d3e47db18aa1d3d0fef63fda0cdb
FavourTy <you@example.com>
2023-10-23 17:43:23 -0700

    mobile codebase

---
+++ mobile/lib/screens/authentication/onboarding_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class OnboardingScreen extends StatefulWidget {
+   static const String routePath = "onboarding_screen";
+  const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ mobile/lib/screens/authentication/splash_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+   static const String routePath = "splash_screen";
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
