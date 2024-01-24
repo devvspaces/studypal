8f13b4b2b06fac05102b16cdc36cfb5d041723c7
VidyaYadavv1519 <vidyayadav617@gmail.com>
2024-01-24 13:35:39 +0530

    Study plan api

--- backend/studypal/views.py
+++ backend/studypal/views.py
@@ -3,6 +3,11 @@ from rest_framework.response import Response
 from rest_framework import status
 from .utils import logger
 from .exceptions import CustomNotFoundException
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from .serializers import *
+from .models import StudyGoal, StudyPlan
+from rest_framework import generics
 
 
 class HelloEndpoint(APIView):
@@ -27,3 +32,26 @@ class ExceptionTestEndpoint(APIView):
                 {"error": "An unexpected error occurred."},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
+
+
+class CreateStudyGoalView(generics.CreateAPIView):
+    queryset = StudyGoal.objects.all()
+    serializer_class = StudyGoalSerializer
+
+
+class DeleteStudyGoalView(generics.DestroyAPIView):
+    queryset = StudyGoal.objects.all()
+    serializer_class = StudyGoalSerializer
+    lookup_field = 'pk'
+
+
+class UpdateStudyGoalView(generics.UpdateAPIView):
+    queryset = StudyGoal.objects.all()
+    serializer_class = StudyGoalSerializer
+    lookup_field = 'pk'
+
+
+class UpdateStudyPlanView(generics.UpdateAPIView):
+    queryset = StudyPlan.objects.all()
+    serializer_class = StudyPlanUpdateSerializer
+    lookup_field = 'pk'
